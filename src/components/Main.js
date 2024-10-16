3c190ff1fe48e30196ac9ac53ed2ed15f4c7e39e
Thilina Bandara <thilinab@sampath.lk>
2024-10-16 17:48:46 +0530

    link added

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,32 +4,48 @@ import { productList } from "../redux/productAction";
 import { useSelector } from "react-redux";
 
 function Main() {
-  const dispatch = useDispatch();
-  let data = useSelector((state) => state.productData)
-    console.log("state-",data);
+    const dispatch = useDispatch();
+
+    let data = useSelector((state) => state.productData);
+    console.log("data---", data);
 
     const product = {
-    name: "Thilina",
-    age: 34,
-    city: "Katunayake",
-  };
+        name: "Thilina",
+        age: 34,
+        city: "Katunayake",
+    };
 
-  return (
-    <div className="App">
-      <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
-      <div>
-        <button onClick={() => dispatch(removeFromCart(product))}>
-          Remove from Cart
-        </button>
-      </div>
-      <div>
-        <button onClick={() => dispatch(emptyFromCart())}>Empty Cart</button>
-      </div>
-      <div>
-        <button onClick={() => dispatch(productList())}>Call Product List</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <button onClick={() => dispatch(addToCart(product))}>
+                Add to Cart
+            </button>
+            <div>
+                <button onClick={() => dispatch(removeFromCart(product))}>
+                    Remove from Cart
+                </button>
+            </div>
+            <div>
+                <button onClick={() => dispatch(emptyFromCart())}>
+                    Empty Cart
+                </button>
+            </div>
+            <div>
+                <button onClick={() => dispatch(productList())}>
+                    Call Product List
+                </button>
+            </div>
+            <div className="product-container">
+                
+                {data.map((item) =><div className="product-item">
+                    {item.name}
+                        <div>
+                        <img src={item.image}/>
+                        </div>
+                    </div>)}
+            </div>
+        </div>
+    );
 }
 
 export default Main;
